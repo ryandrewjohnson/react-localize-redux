15ece2afaaa7bc529cfee040f47bf808c7f78e8d
ramyma <ramy.mahmoudi@gmail.com>
2018-10-17 20:26:02 +0200

    Added hoist-non-react-statics to withLocalized HOC (#130)
    * -Added hoist-non-react-statics to withLocalized HOC
    
    * -Added test case for hoisting statics in withLoclize HOC

--- src/withLocalize.js
+++ src/withLocalize.js
@@ -1,16 +1,19 @@
 import React, { Component, type ComponentType } from 'react';
+import hoistNonReactStatic from 'hoist-non-react-statics';
 import { LocalizeContext, type LocalizeContextProps } from './LocalizeContext';
 
 export function withLocalize<Props: {}>(
   WrappedComponent: ComponentType<Props>
 ): ComponentType<$Diff<Props, { ...LocalizeContextProps }>> {
-  const LocalizedComponent = (props: Props) => {
-    return (
-      <LocalizeContext.Consumer>
-        {context => <WrappedComponent {...context} {...props} />}
-      </LocalizeContext.Consumer>
-    );
-  };
-
+  class LocalizedComponent extends Component {
+    render() {
+      return (
+        <LocalizeContext.Consumer>
+          {context => <WrappedComponent {...context} {...this.props} />}
+        </LocalizeContext.Consumer>
+      );
+    }
+  }
+  hoistNonReactStatic(LocalizedComponent, WrappedComponent);
   return LocalizedComponent;
 }
