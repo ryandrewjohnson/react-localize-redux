6cd985e85e76a24a0a0f51d4b7e0d85679803bb8
Ryan Johnson <ryan@shakainteractive.com>
2019-02-18 21:04:00 -0500

    working rollup version

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/index.ts',
     external: ['react', 'redux'],
@@ -14,24 +13,18 @@ export default [
       format: 'umd'
     },
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      typescript() // so Rollup can convert TypeScript to JavaScript
+      resolve(), 
+      commonjs(), 
+      typescript()
     ]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/index.ts',
     external: ['react', 'redux'],
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      typescript() // so Rollup can convert TypeScript to JavaScript
+      resolve(), 
+      commonjs(), 
+      typescript() 
     ],
     output: [
       { file: pkg.main, format: 'cjs' },
--- src/withLocalize.tsx
+++ src/withLocalize.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { LocalizeContext } from './LocalizeContext';
-const hoistNonReactStatic = require('hoist-non-react-statics');
+// const hoistNonReactStatic = require('hoist-non-react-statics');
+import hoistNonReactStatic from 'hoist-non-react-statics';
 
 export function withLocalize(WrappedComponent) {
   class LocalizedComponent extends Component {
